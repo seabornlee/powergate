9d12232079328392fbeecd2798085fd6b56d487b
Ignacio Hagopian <jsign.uy@gmail.com>
2020-08-26 10:29:59 -0300

    retry lotus conn on tests
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/ldevnet.go
+++ tests/ldevnet.go
@@ -76,7 +76,7 @@ func LaunchDevnetDocker(t TestingTWithCleanup, numMiners int, ipfsMaddr string, 
 // CreateLocalDevnetWithIPFS creates a local devnet connected to an IPFS node.
 func CreateLocalDevnetWithIPFS(t TestingTWithCleanup, numMiners int, ipfsMaddr string, mountVolumes bool) (*apistruct.FullNodeStruct, address.Address, []address.Address) {
 	lotusDevnet := LaunchDevnetDocker(t, numMiners, ipfsMaddr, mountVolumes)
-	c, cls, err := lotus.New(util.MustParseAddr("/ip4/127.0.0.1/tcp/"+lotusDevnet.GetPort("7777/tcp")), "", 1)
+	c, cls, err := lotus.New(util.MustParseAddr("/ip4/127.0.0.1/tcp/"+lotusDevnet.GetPort("7777/tcp")), "", 2)
 	require.NoError(t, err)
 	t.Cleanup(func() { cls() })
 	ctx := context.Background()
