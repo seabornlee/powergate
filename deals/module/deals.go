281170107f2a3d46168d3015509b9785446b5f04
Ignacio Hagopian <jsign.uy@gmail.com>
2020-09-03 19:43:03 -0300

    more progress
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- deals/module/deals.go
+++ deals/module/deals.go
@@ -17,6 +17,7 @@ import (
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/apistruct"
 	"github.com/filecoin-project/lotus/chain/types"
+	marketevents "github.com/filecoin-project/lotus/markets/loggers"
 	"github.com/filecoin-project/specs-actors/actors/abi"
 	"github.com/filecoin-project/specs-actors/actors/abi/big"
 	"github.com/ipfs/go-cid"
@@ -150,12 +151,12 @@ func (m *Module) Store(ctx context.Context, waddr string, dataCid cid.Cid, piece
 // Fetch fetches deal data to the underlying blockstore of the Filecoin client.
 // This API is meant for clients that use external implementations of blockstores with
 // their own API, e.g: IPFS.
-func (m *Module) Fetch(ctx context.Context, waddr string, payloadCid cid.Cid, pieceCid *cid.Cid) error {
-	return m.retrieve(ctx, waddr, payloadCid, pieceCid, nil)
+func (m *Module) Fetch(ctx context.Context, waddr string, payloadCid cid.Cid, pieceCid *cid.Cid, miners []string) (<-chan marketevents.RetrievalEvent, error) {
+	return m.retrieve(ctx, waddr, payloadCid, pieceCid, miners, nil)
 }
 
 // Retrieve retrieves Deal data.
-func (m *Module) Retrieve(ctx context.Context, waddr string, payloadCid cid.Cid, pieceCid *cid.Cid, CAREncoding bool) (io.ReadCloser, error) {
+func (m *Module) Retrieve(ctx context.Context, waddr string, payloadCid cid.Cid, pieceCid *cid.Cid, miners []string, CAREncoding bool) (io.ReadCloser, error) {
 	rf, err := ioutil.TempDir(m.cfg.ImportPath, "retrieve-*")
 	if err != nil {
 		return nil, fmt.Errorf("creating temp dir for retrieval: %s", err)
@@ -165,39 +166,96 @@ func (m *Module) Retrieve(ctx context.Context, waddr string, payloadCid cid.Cid,
 		IsCAR: CAREncoding,
 	}
 
-	if err := m.retrieve(ctx, waddr, payloadCid, pieceCid, &ref); err != nil {
+	events, err := m.retrieve(ctx, waddr, payloadCid, pieceCid, miners, &ref)
+	if err != nil {
 		return nil, fmt.Errorf("retrieving from lotus: %s", err)
 	}
-
+	for e := range events {
+		if e.Err != "" {
+			return nil, fmt.Errorf("in progress retrieval error: %s", e.Err)
+		}
+	}
 	f, err := os.Open(ref.Path)
 	if err != nil {
 		return nil, fmt.Errorf("opening retrieved file: %s", err)
 	}
+
 	return &autodeleteFile{File: f}, nil
 }
 
-func (m *Module) retrieve(ctx context.Context, waddr string, payloadCid cid.Cid, pieceCid *cid.Cid, ref *api.FileRef) error {
+func (m *Module) retrieve(ctx context.Context, waddr string, payloadCid cid.Cid, pieceCid *cid.Cid, miners []string, ref *api.FileRef) (<-chan marketevents.RetrievalEvent, error) {
 	addr, err := address.NewFromString(waddr)
 	if err != nil {
-		return err
+		return nil, fmt.Errorf("parsing wallet address: %s", err)
 	}
-	offers, err := m.api.ClientFindData(ctx, payloadCid, pieceCid)
-	if err != nil {
-		return err
+
+	// Ask each miner about costs and information about retrieving this data.
+	var offers []api.QueryOffer
+	for _, mi := range miners {
+		a, err := address.NewFromString(mi)
+		if err != nil {
+			log.Infof("parsing miner address: %s", err)
+		}
+		qo, err := m.api.ClientMinerQueryOffer(ctx, a, payloadCid, pieceCid)
+		if err != nil {
+			log.Infof("asking miner %s query-offer failed: %s", m, err)
+			continue
+		}
+		offers = append(offers, qo)
 	}
+
+	// If no miners available, fail.
 	if len(offers) == 0 {
-		return ErrRetrievalNoAvailableProviders
+		return nil, ErrRetrievalNoAvailableProviders
 	}
-	for _, o := range offers {
-		err = m.api.ClientRetrieve(ctx, o.Order(addr), ref)
+
+	// Sort received options by price.
+	sort.Slice(offers, func(a, b int) bool { return offers[a].MinPrice.LessThan(offers[b].MinPrice) })
+
+	out := make(chan marketevents.RetrievalEvent, 1)
+	var events <-chan marketevents.RetrievalEvent
+
+	// Try with sorted miners until we got in the process of receiving data.
+	var o api.QueryOffer
+	for _, o = range offers {
+		events, err = m.api.ClientRetrieveWithEvents(ctx, o.Order(addr), ref)
 		if err != nil {
 			log.Infof("fetching/retrieving cid %s from %s: %s", payloadCid, o.Miner, err)
 			continue
 		}
-		m.recordRetrieval(waddr, o)
-		return nil
+		break
 	}
-	return fmt.Errorf("couldn't retrieve data from any miners, last miner err: %s", err)
+
+	go func() {
+		defer close(out)
+		// Redirect received events to the output channel
+		var errored, canceled bool
+	Loop:
+		for {
+			select {
+			case <-ctx.Done():
+				log.Infof("in progress retrieval canceled")
+				canceled = true
+				break
+			case e, ok := <-events:
+				if !ok {
+					break Loop
+				}
+				if e.Err != "" {
+					log.Infof("in progress retrieval errored: %s", err)
+					errored = true
+				}
+				out <- e
+			}
+		}
+
+		// Only register retrieval if successful
+		if !errored && !canceled {
+			m.recordRetrieval(waddr, o)
+		}
+	}()
+
+	return out, nil
 }
 
 // GetDealStatus returns the current status of the deal, and a flag indicating if the miner of the deal was slashed.
